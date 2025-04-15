bb2c92c3f5b614d5762ba23421602574cbd32231
lzl2040 <2040171586@qq.com>
2025-04-15 15:08:01 +0000

    step_120k_wo_img_aug

--- finetune_on_40G.sh
+++ finetune_on_40G.sh
@@ -5,8 +5,8 @@ python lerobot/scripts/dps_train.py \
     --dataset.repo_id="any/simulted" \
     --wandb.enable=true \
     --wandb.project="pi0-ft-simulated" \
-    --job_name="pi0-04-16-ft-libero-goal-bs-4-8gpu-gra-acc-4-with-lr-decay-warm-1k-w-img-aug-1st" \
+    --job_name="pi0-04-16-ft-libero-goal-bs-4-8gpu-gra-acc-4-with-lr-decay-warm-1k-wo-img-aug-1st" \
     --log_dir="/mnt/wangxiaofa/logs" \
-    --output_dir="/mnt/wangxiaofa/pi0-ft-simulated/0416-ft-libero-goal-bs-4-8gpu-gra-acc-4-with-lr-decay-warm-1k-w-img-aug-1st" \
+    --output_dir="/mnt/wangxiaofa/pi0-ft-simulated/0416-ft-libero-goal-bs-4-8gpu-gra-acc-4-with-lr-decay-warm-1k-wo-img-aug-1st" \
     --steps=120_000 \
-    --dataset.image_transforms.enable=true
+    --dataset.image_transforms.enable=false
