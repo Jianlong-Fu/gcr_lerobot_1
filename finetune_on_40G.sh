60b39d50d308341222918ff21819d4e79887a12f
lzl2040 <2040171586@qq.com>
2025-04-24 13:38:50 +0000

    update

--- finetune_on_40G.sh
+++ finetune_on_40G.sh
@@ -1,13 +1,13 @@
 python lerobot/scripts/dps_train.py \
     --deepspeed="./ds_zero2.json" \
     --policy.type="pi0" \
-    --dataset.root="/mnt/wangxiaofa/robot_dataset/lerobot-format/bridge_orig_lerobot/" \
+    --dataset.root="/mnt/wangxiaofa/robot_dataset/lerobot-format/bridge_orig_lerobot" \
     --dataset.repo_id="any/simulted" \
     --wandb.enable=true \
     --wandb.project="pi0-ft-simulated" \
-    --job_name="pi0-04-24-ft-bridge-bs-4-4gpu-gra-acc-4-with-lr-decay-warm-2k-1st" \
+    --job_name="pi0-04-24-ft-bridge-bs-16-2gpu-gra-acc-4-with-lr-decay-warm-2k-1st" \
     --log_dir="/mnt/wangxiaofa/logs" \
-    --output_dir="/mnt/wangxiaofa/pi0-ft-simulated/0424-ft-bridge-bs-4-4gpu-gra-acc-4-with-lr-decay-warm-2k-1st" \
+    --output_dir="/mnt/wangxiaofa/pi0-ft-simulated/0424-ft-bridge-bs-16-2gpu-gra-acc-4-with-lr-decay-warm-2k-1st" \
     --steps=240_000 \
     # --dataset.image_transforms.enable=true \
     # --dataset.wrist_image_transforms.enable=true
