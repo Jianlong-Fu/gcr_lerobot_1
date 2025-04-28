678cd9d650609123bedffb8753bf8d9f79766d75
lzl2040 <2040171586@qq.com>
2025-04-28 16:05:19 +0000

    ft simpler_fractal_180K_wo_random_order_and_crop_2_80G_A100

--- finetune_on_40G.sh
+++ finetune_on_40G.sh
@@ -1,13 +1,13 @@
 python lerobot/scripts/dps_train.py \
     --deepspeed="./ds_zero2_40G.json" \
     --policy.type="pi0" \
-    --dataset.root="/mnt/wangxiaofa/robot_dataset/lerobot-format/simpler_bridge" \
+    --dataset.root="/mnt/wangxiaofa/robot_dataset/lerobot-format/simpler_fractal" \
     --dataset.repo_id="any/simulted" \
     --wandb.enable=true \
     --wandb.project="pi0-ft-simulated" \
-    --job_name="pi0-04-27-ft-simpler-bridge-bs-4-8gpu-gra-acc-4-with-lr-decay-warm-1.5k-w-img-aug-no-random-order-and-crop-1st" \
+    --job_name="pi0-04-28-ft-simpler-fractal-bs-16-2gpu-gra-acc-4-with-lr-decay-warm-1.5k-w-img-aug-no-random-order-and-crop-1st" \
     --log_dir="/mnt/wangxiaofa/logs" \
-    --output_dir="/mnt/wangxiaofa/pi0-ft-simulated/0427-ft-simpler-bridge-bs-4-8gpu-gra-acc-4-with-lr-decay-warm-1.5k-w-img-aug-no-random-order-and-crop-2rd" \
+    --output_dir="/mnt/wangxiaofa/pi0-ft-simulated/0428-ft-simpler-fractal-bs-16-2gpu-gra-acc-4-with-lr-decay-warm-1.5k-w-img-aug-no-random-order-and-crop-1st" \
     --steps=180_000 \
     --dataset.image_transforms.enable=true \
     # --dataset.wrist_image_transforms.enable=true
