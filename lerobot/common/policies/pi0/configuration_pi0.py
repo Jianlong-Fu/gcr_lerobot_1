6a148ac63b9d1286591e2f3a8b01ff5d3056ea77
lzl2040 <2040171586@qq.com>
2025-05-19 09:10:47 +0000

    update ft_vision

--- lerobot/common/policies/pi0/configuration_pi0.py
+++ lerobot/common/policies/pi0/configuration_pi0.py
@@ -58,7 +58,7 @@ class PI0Config(PreTrainedConfig):
 
     # Finetuning settings
     # freeze_vision_encoder: bool = True
-    freeze_vision_encoder: bool = True
+    freeze_vision_encoder: bool = False
     train_expert_only: bool = False
     train_state_proj: bool = True
 
