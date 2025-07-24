19e637bee52e412f200abb24e912d1092653c669
lzlglory <lizuolei24@ime.ac.cn>
2025-07-24 02:57:29 +0000

    task_5

--- lerobot/scripts/dps_train.py
+++ lerobot/scripts/dps_train.py
@@ -361,23 +361,23 @@ def train(cfg: TrainPipelineConfig):
             os.makedirs(cfg.output_dir, exist_ok=True)
             
             client_state['step'] = step
-            # 获取 LoRA 模型引用
-            lora_module = model_engine.module.model.paligemma_with_expert.paligemma
-            assert isinstance(lora_module, PeftModel)
+            # # 获取 LoRA 模型引用
+            # lora_module = model_engine.module.model.paligemma_with_expert.paligemma
+            # assert isinstance(lora_module, PeftModel)
 
-            # 1. 暂时合并并卸载 LoRA adapter
-            merged_model = lora_module.merge_and_unload()
+            # # 1. 暂时合并并卸载 LoRA adapter
+            # merged_model = lora_module.merge_and_unload()
 
-            # 2. 替换模型中原来的模块（只用于保存）
-            model_engine.module.model.paligemma_with_expert.paligemma = merged_model
+            # # 2. 替换模型中原来的模块（只用于保存）
+            # model_engine.module.model.paligemma_with_expert.paligemma = merged_model
 
             # 3. 执行保存（保存的是合并后的模型）
             model_engine.save_checkpoint(
                 save_dir=cfg.output_dir,
                 client_state=client_state
             )
-            # 4. 恢复原始 LoRA 模型（还原）
-            model_engine.module.model.paligemma_with_expert.paligemma = lora_module
+            # # 4. 恢复原始 LoRA 模型（还原）
+            # model_engine.module.model.paligemma_with_expert.paligemma = lora_module
             # torch.save(client_state, os.path.join(checkpoint_dir, "metadata.pt"))
             # update_last_checkpoint(checkpoint_dir)
         
