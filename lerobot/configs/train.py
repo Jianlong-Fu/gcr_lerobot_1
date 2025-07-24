0a5333ebb71f7435cbf9cc6e2be1143086fb0333
lzlglory <lizuolei24@ime.ac.cn>
2025-07-24 02:59:11 +0000

    update save_freq

--- lerobot/configs/train.py
+++ lerobot/configs/train.py
@@ -54,7 +54,7 @@ class TrainPipelineConfig(HubMixin):
     log_freq: int = 400 # * gradient steps
     save_checkpoint: bool = True
     # Checkpoint is saved every `save_freq` training iterations and after the last training step.
-    save_freq: int = 200
+    save_freq: int = 500
     use_policy_training_preset: bool = True
     optimizer: OptimizerConfig | None = None
     scheduler: LRSchedulerConfig | None = None
