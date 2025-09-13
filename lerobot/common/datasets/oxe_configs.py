95de632f4bbfbdb09c224cd26f4db5891a94a377
lzlglory <lizuolei24@ime.ac.cn>
2025-09-13 16:54:01 +0000

    update

--- lerobot/common/datasets/oxe_configs.py
+++ lerobot/common/datasets/oxe_configs.py
@@ -766,5 +766,13 @@ OXE_DATASET_CONFIGS = {
         "action_encoding": ActionEncoding.EEF_POS,
     },
     
+    "pizza_long_sub1_hz_15": {
+        "image_obs_keys": {"primary": "right_rgb", "secondary": "top_rgb", "wrist": "wrist_rgb"},
+        "depth_obs_keys": {"primary": None, "secondary": None, "wrist": None},
+        "state_obs_keys": ["EEF_state", None, "gripper_state"],
+        "state_encoding": StateEncoding.POS_EULER,
+        "action_encoding": ActionEncoding.EEF_POS,
+    },
+    
     
 }
