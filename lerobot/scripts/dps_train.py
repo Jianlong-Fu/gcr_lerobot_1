9333f668f41c08e884629a966ffc7a43a379e7ad
lzlglory <lizuolei24@ime.ac.cn>
2025-09-15 12:14:11 +0000

    update

--- lerobot/scripts/dps_train.py
+++ lerobot/scripts/dps_train.py
@@ -203,7 +203,7 @@ def train(cfg: TrainPipelineConfig):
         cfg=cfg.policy,
         device='cpu',
         ds_meta=dataset.meta,
-        weight_pt_path="/mnt/wangxiaofa/pi0-ft-simulated/0914-pizza-long-sub1-hz-15-wo-state/global_step15000/mp_rank_00_model_states.pt"
+        weight_pt_path="/mnt/wangxiaofa/pi0-ft-simulated/0914-pizza-long-sub3-hz-2.5-w-state/global_step15000/mp_rank_00_model_states.pt"
     )
     
     logger.info("Policy model created...")
