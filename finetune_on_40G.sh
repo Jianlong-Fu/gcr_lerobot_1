3e81ca94d55d39f58fd92fd1a3c8cdfded8e7d48
lzlglory <lizuolei24@ime.ac.cn>
2025-09-22 08:17:26 +0000

    update

--- finetune_on_40G.sh
+++ finetune_on_40G.sh
@@ -9,7 +9,7 @@ python lerobot/scripts/dps_train.py \
     --wandb.enable=false \
     --resume=false \
     --wandb.project="pi0-ft-simulated" \
-    --job_name="0919-pizza-long-full-hz-2.5-w-state" \
+    --job_name="0919-pizza-long-full-hz-2.5-wo-state-resume" \
     --log_dir="/mnt/wangxiaofa/logs" \
-    --output_dir="/mnt/wangxiaofa/pi0-ft-simulated/0919-pizza-long-full-hz-2.5-w-state" \
+    --output_dir="/mnt/wangxiaofa/pi0-ft-simulated/0919-pizza-long-full-hz-2.5-wo-state-resume" \
     --steps=300_000
